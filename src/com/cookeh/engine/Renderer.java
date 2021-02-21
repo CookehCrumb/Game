3318a964b404075451a3b8f3a4abca27b4869e9d
CookehCrumb <CookehCrumb@gmail.com>
2021-02-20 23:35:44 -0500

    Render objects

--- src/com/cookeh/engine/Renderer.java
+++ src/com/cookeh/engine/Renderer.java
@@ -93,4 +93,33 @@ public class Renderer
 			}
 		}
 	}
+	
+	public void drawFillRect(int offX, int offY, int width, int height, int color)
+	{
+		//Culling Render Optimizations
+		if(offX < -width) return;
+		if(offY < -height) return;
+		if(offX >= pW) return;
+		if(offY >= pH) return;
+						
+		int newX = 0;
+		int newY = 0;
+		int newWidth = width;
+		int newHeight = height;
+						
+		//Clipping Render Optimizations
+		if(offX < 0) { newX -= offX; }
+		if(offY < 0) { newY -= offY; }
+		if(newWidth + offX > pW) { newWidth -= newWidth + offX - pW; }		
+		if(newHeight + offY > pH) { newHeight -= newHeight + offY - pH; }
+						
+		
+		for(int y = newY; y <= newHeight; y++) 
+		{
+			for(int x = newX; x <= newWidth; x++)
+			{
+				setPixel(x + offX, y + offY, color);
+			}
+		}
+	}
 }
